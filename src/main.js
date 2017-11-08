fbbd8f50c4e2c49d3349bd1b2bce8995c54a676b
nek neyugn <nek@neks-mbp.home>
2017-11-08 20:01:15 +0100

    load an html onto the app gui

--- src/main.js
+++ src/main.js
@@ -4,10 +4,16 @@ const electron = require('electron')
 const app = electron.app
 const gui = electron.BrowserWindow
 
+let mainWindow
 app.on('ready', _ => {
   console.log('Electron app ready!')
-  new gui({
+  mainWindow = new gui({
     height: 400,
     width: 400
+  })
+  mainWindow.loadURL(`file://${__dirname}/countdown.html`)
+  mainWindow.on('close', _ => {
+    console.log('Window closed!')
+    mainWindow = null
   })
 })
