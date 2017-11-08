c684632bee781ead4a4b4a7d9be614b6b06d4fb7
nek neyugn <nek@neks-mbp.home>
2017-11-08 19:50:07 +0100

    add a window

--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ console.log('Starting up...')
 const electron = require('electron')
 
 const app = electron.app
+const gui = electron.BrowserWindow
 
 app.on('ready', _ => {
   console.log('Electron app ready!')
+  new gui({
+    height: 400,
+    width: 400
+  })
 })
