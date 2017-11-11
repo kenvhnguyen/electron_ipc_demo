901f737fb048c8596b73e9af30a898556b1db720
nek neyugn <nek@192.168.2.12>
2017-11-11 11:34:22 +0100

    installed dev-tools to see logging of embedded module countdown.js

--- src/main.js
+++ src/main.js
@@ -4,14 +4,18 @@ const electron = require('electron')
 const app = electron.app
 const gui = electron.BrowserWindow
 
+const countdown = require('./countdown')
+
 let mainWindow
 app.on('ready', _ => {
   console.log('Electron app ready!')
+
   mainWindow = new gui({
     height: 400,
     width: 400
   })
   mainWindow.loadURL(`file://${__dirname}/countdown.html`)
+  countdown()
   mainWindow.on('close', _ => {
     console.log('Window closed!')
     mainWindow = null
--- src/renderer.js
+++ src/renderer.js
@@ -1,1 +1,6 @@
 console.log('In renderer!')
+window.onload=function() {
+    document.getElementById('start').addEventListener('click', _=>{
+        console.log('start button clicked!')
+    })  
+}
